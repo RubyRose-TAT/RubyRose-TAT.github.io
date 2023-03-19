941252e81a35c3e992f9f94f571b90d89e86cbe7
RubyRose-TAT <rubyrosetat@gmail.com>
2023-03-20 03:21:18 +0800

    Site updated: 2023-03-20 03:21:18

--- js/live2d.js
+++ js/live2d.js
@@ -1,17 +1,17 @@
 addEventListener('DOMContentLoaded', function () {
   let models = [
     {
-      width: '500',
-      height: '437.5',
-      left: '-100px',
-      bottom: '-20px',
+      width: '285',
+      height: '285',
+      left: '-5px',
+      bottom: '-6px',
       role: 'https://npm.elemecdn.com/chenyfan-oss@2.0.2/lafei_4.model3.json',
       background: '',
       opacity: 1,
       mobile: false,
-      scale: 0.75,
+      scale: 0.04,
       draggable: false,
-      pierceThrough: true,
+      pierceThrough: false,
     }
   ]
   new Live2dLoader(models)
